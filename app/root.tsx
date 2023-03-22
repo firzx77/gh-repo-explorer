f91f13ac7c9293d9ba13a5ec46e051c15fa29a7e
Faris <FArIZzX77@gmail.com>
2023-03-22 10:34:53 +0700

    update title

--- app/root.tsx
+++ app/root.tsx
@@ -12,7 +12,7 @@ import styles from "./style.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "GitHub Repo Explorer",
   viewport: "width=device-width,initial-scale=1",
 });
 
